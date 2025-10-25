4925dc75fa469568f93b4b24ed2f73cbec974ac4
mike <mikeadouglass@gmail.com>
2025-10-25 17:22:13 -0400

    Update for jakarta and unspring

--- bw-sometime-database/src/main/java/org/bedework/sometime/database/migrations/FlywayDatabaseMigrations.java
+++ bw-sometime-database/src/main/java/org/bedework/sometime/database/migrations/FlywayDatabaseMigrations.java
@@ -3,29 +3,22 @@
  */
 package org.bedework.sometime.database.migrations;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.inject.Named;
-
 import org.flywaydb.core.Flyway;
 
+import javax.annotation.PostConstruct;
 
 /**
  * Flyway backed class responsible for executing database migration.
  * 
- * Depends on injection of a {@link Flyway} instance.
- * 
  * @author Nicholas Blair
  */
-@Named
 public class FlywayDatabaseMigrations {
-
 	private Flyway flyway;
+
 	/**
 	 * @param flyway the {@link Flyway} to be used by {@link #runMigrations()}
 	 */
-	@Inject
-	public void setFlyway(Flyway flyway) {
+	public void setFlyway(final Flyway flyway) {
 		this.flyway = flyway;
 	}
 	/**
--- bw-sometime-database/src/test/java/org/bedework/sometime/database/migrations/TestConfiguration.java
+++ bw-sometime-database/src/test/java/org/bedework/sometime/database/migrations/TestConfiguration.java
@@ -4,23 +4,16 @@
 package org.bedework.sometime.database.migrations;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 /**
- * {@link Configuration} for {@link FlywayDatabaseMigrationsTest}.
+ * Configuration for FlywayDatabaseMigrationsTest.
  * 
  * @author Nicholas Blair
  */
-@Configuration
 class TestConfiguration {
-
-	@Bean Flyway flyway() {
-		SingleConnectionDataSource dataSource = new SingleConnectionDataSource("jdbc:hsqldb:mem:bw", "sa", "", true);
-		Flyway flyway = new Flyway();
-		flyway.setDataSource(dataSource);
-		flyway.setLocations("db/hsqldb", "db/migration");
-		return flyway;
+	public Flyway flyway() {
+		return new Flyway(Flyway.configure()
+														.dataSource("jdbc:hsqldb:mem:bw", "sa", "")
+														.locations("db/hsqldb", "db/migration"));
 	}
 }
